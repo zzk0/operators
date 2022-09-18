08ef35f2c65b19753b3354a644bd5bafd8bf8958
zzk0 <zzk9460@gmail.com>
2022-09-18 15:36:54 +0800

    format output: fix markdown title

--- scripts/cpuinfo.sh
+++ scripts/cpuinfo.sh
@@ -4,18 +4,18 @@ set -exo
 
 output_file=$1
 
-echo '##CPU' >> $output_file
-lscpu > $output_file
+echo '## CPU' > $output_file
+lscpu >> $output_file
 
-echo '##Cache' >> $output_file
+echo '## Cache' >> $output_file
 echo '```' >> $output_file
 ./csrc/cache_info >> $output_file
 echo '```' >> $output_file
 
-echo '##Memory' >> $output_file
+echo '## Memory' >> $output_file
 free -mh >> $output_file
 
-echo '##Peak Performance' >> $output_file
+echo '## Peak Performance' >> $output_file
 echo '```' >> $output_file
 ./cpufp --thread_pool=[0] | tail -n +3 >> $output_file
 echo '```' >> $output_file
