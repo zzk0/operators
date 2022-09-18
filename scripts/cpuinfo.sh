42f85cfb1a13cd36c7d8c58158f33ca2bfa159f8
zzk0 <zzk9460@gmail.com>
2022-09-18 15:30:40 +0800

    format output:

--- scripts/cpuinfo.sh
+++ scripts/cpuinfo.sh
@@ -3,14 +3,21 @@
 set -exo
 
 output_file=$1
-echo '```' >> $output_file
-echo 'lscpu: ' >> $output_file
+
+echo '##CPU' >> $output_file
 lscpu > $output_file
-echo 'free -mh' >> $output_file
-free -mh >> $output_file
-echo 'Cache Info' >> $output_file
+
+echo '##Cache' >> $output_file
+echo '```' >> $output_file
 ./csrc/cache_info >> $output_file
-echo 'Peak Performance' >> $output_file
+echo '```' >> $output_file
+
+echo '##Memory' >> $output_file
+free -mh >> $output_file
+
+echo '##Peak Performance' >> $output_file
+echo '```' >> $output_file
 ./cpufp --thread_pool=[0] | tail -n +3 >> $output_file
 echo '```' >> $output_file
+
 cat $output_file
